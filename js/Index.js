1c9b5c0aeb773130a1c1fc425fde9e314082d8b6
IAmMonmoy <iammonmoy@gmail.com>
2018-02-07 17:29:20 +0600

    sliding navigation bar complete

---
+++ js/Index.js
@@ -0,0 +1,27 @@
+document.addEventListener('DOMContentLoaded',function(event){
+    //navigation bar will be closed at start
+   
+});
+
+
+//for opening and closing navigation start
+var openSidebarButton = document.getElementById("openSidebarButton");
+var closeButton = document.getElementById("closeButton");
+openSidebarButton.addEventListener('click',openSidebarButtonClicked);
+closeButton.addEventListener('click',closeButtonClicked);
+
+
+
+function openSidebarButtonClicked()
+{
+    document.getElementsByClassName('navbar')[0].style.width = '250px';
+    document.getElementsByClassName('bodyContent')[0].style.marginLeft = '250px';
+}
+
+function closeButtonClicked()
+{
+    var x = document.getElementsByClassName('navbar')[0].style.width = '0px';
+    document.getElementsByClassName('bodyContent')[0].style.marginLeft = '0px';
+}
+
+//for opening and closing navigation end
